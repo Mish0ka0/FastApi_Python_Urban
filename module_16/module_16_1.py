1ef4ead4cd5b2b16eca63c6769528d17d26bf21c
Mish0ka0 <sega.vasilev.2014@mail.ru>
2025-01-14 11:18:40 +0300

    веб приложение fastapi

--- module_16/module_16_1.py
+++ module_16/module_16_1.py
@@ -4,5 +4,20 @@ app = FastAPI()
 
 
 @app.get("/")
-async def main_page() -> dict:
-    return {"message": "Главная страница"}
+async def main_page() -> str:  # статичный маршрут
+    return "Главная страница"
+
+
+@app.get("/user/admin")
+async def admin_panel() -> str:  # статичный маршрут
+    return "Вы вошли как администратор"
+
+
+@app.get("/user/{user_id}")
+async def user_number(user_id: int) -> str:  # динамический машрут с параметром в пути
+    return f'Вы вошли как пользователь № {user_id}'
+
+
+@app.get("/user")
+async def user_info(username: str, age: int) -> str:  # динамический маршрут с параметрами запроса(query parameters)
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
